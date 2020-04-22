c790795cbb1c5af2221f1ceda3c6bb1558fa53a4
Akil Griffiths <akilgg@gmail.com>
2020-04-22 16:46:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,6 +7,12 @@ def loop_message_n_times(string, n = 1)
   n.times {puts string}
 end
 
-def output_array()
+def output_array(string, n = 1)
+n.times {puts string}
+end
 
+def return_string_array(array)
+  new_array = []
+new_array << array.to_s
+new_array
 end
