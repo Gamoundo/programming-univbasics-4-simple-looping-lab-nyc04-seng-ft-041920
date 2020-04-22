c88370fb7f366f5c65b8af9abd6c8928823e1112
Akil Griffiths <akilgg@gmail.com>
2020-04-22 15:57:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5times puts string
+end
+
+def loop_message_n_times(string, n = 1)
+  ntimes puts string
+end
+
+def output_array()
+
+end
